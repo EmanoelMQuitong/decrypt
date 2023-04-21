940bff93f3ff2e8a08f49ccb70ccb4b97118a815
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-22 00:21:26 +0800

    initially encrypted message is being decrypted as 'Decrypted_string'.It is declared which text are replaced with respective characters

--- Decrypt.py
+++ Decrypt.py
@@ -6,3 +6,4 @@ from colored import fg
 color = fg('yellow')
 
 encrypted_string = input("Enter a encrypted text to decrypt: ")
+decrypted_string = encrypted_string.replace('*','a').replace('&','e').replace('#','i').replace('+','o').replace('!','u')
