25afaeae9100580836ef8cb68e2954218970b832
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-22 00:24:28 +0800

    Print is created. Text colors and text art is applied for creativity purposes only.

--- Decrypt.py
+++ Decrypt.py
@@ -7,3 +7,5 @@ color = fg('yellow')
 
 encrypted_string = input("Enter a encrypted text to decrypt: ")
 decrypted_string = encrypted_string.replace('*','a').replace('&','e').replace('#','i').replace('+','o').replace('!','u')
+
+print('\n', "Decrypted text: ", '\n', color + pyfiglet.figlet_format(decrypted_string, font= "digital"))
